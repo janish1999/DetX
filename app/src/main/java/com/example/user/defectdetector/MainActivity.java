6daaa26bb37e2e83bb6736072d490b664a695383
Janish <jsjanish.1999@gmail.com>
2019-03-03 12:15:08 +0530

    Second Commit

--- app/src/main/java/com/example/user/defectdetector/MainActivity.java
+++ app/src/main/java/com/example/user/defectdetector/MainActivity.java
@@ -7,18 +7,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 1500;
+    private int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent (MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+        if (flag == 0) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+            flag = 1;
+        }
     }
 }
